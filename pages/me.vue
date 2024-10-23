e77340bafdb28fcb70f169bfda80295ec712c5bf
zSnails <erizojuan33@gmail.com>
2024-10-23 12:04:57 -0600

    feat: profile and settings pages

---
+++ pages/me.vue
@@ -0,0 +1,6 @@
+<template>
+  <p1>WIP</p1>
+</template>
+<script setup lang="ts">
+
+</script>
---
+++ pages/settings.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script setup lang="ts">
+const user = useSupabaseUser();
+
+</script>
