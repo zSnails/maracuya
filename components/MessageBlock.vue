5dcfef4d5f7551e12354065dc38eb0f71c0576f3
zSnails <erizojuan33@gmail.com>
2024-10-23 12:04:12 -0600

    fix: update the message block id

--- components/MessageBlock.vue
+++ components/MessageBlock.vue
@@ -1,11 +1,8 @@
 <template>
-  <div :id="`${message.sent_by}-${message.sent_to}-${message.id}`"
+  <div :id="`${message.sent_by}-${message.conversation}-${message.id}`"
     class="rounded-box bg-base-200 p-5 m-2 flex flex-row items-start justify-between"
     :class="{ 'animate-pulse': !!!message.id }">
     <div class="flex flex-row">
-      <!-- <div class="avatar">
-        <img src="" alt="">
-      </div> -->
       <section class="flex flex-col items-start">
         <h1 class="text-xl font-bold">{{ message.sent_by }}</h1>
         <p>{{ message.content }}</p>
