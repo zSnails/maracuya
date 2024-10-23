01903abc1bf2aadcdf500f6f8e61e7078bab7e8a
zSnails <erizojuan33@gmail.com>
2024-10-23 01:50:02 -0600

    feat: login page

---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="m-auto flex shadow-xl flex-col gap-2 items-start bg-base-200 w-fit h-fit p-5 rounded-box">
+      <h1 class="font-bold text-2xl">Log In</h1>
+      <form @submit.prevent="login" class="form flex flex-col gap-5" action="">
+        <div class="form-control flex flex-col gap-2">
+          <label class="input input-bordered flex items-center gap-2">
+            <EnvelopeIcon class="size-6" />
+            <input v-model="loginInfo.email" type="email" class="grow" placeholder="Email" required />
+          </label>
+          <label class="input input-bordered flex items-center gap-2">
+            <LockClosedIcon class="size-6" />
+            <input v-model="loginInfo.password" type="password" class="grow" placeholder="Password" required />
+          </label>
+        </div>
+
+        <div class="form-control">
+          <button class="btn btn-primary" type="submit">Log In</button>
+          <NuxtLink class="link" href="/register">Don't have an account?</NuxtLink>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { LockClosedIcon, EnvelopeIcon } from '@heroicons/vue/16/solid';
+
+const supabase = useSupabaseClient();
+const loginInfo = ref<{ email: string; password: string; }>({ email: '', password: '' });
+
+const login = async () => {
+  const status = await supabase.auth.signInWithPassword(loginInfo.value);
+  console.table(status);
+}
+
+
+</script>
