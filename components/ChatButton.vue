4fcdc5e3ea690b120c3212aaf7c60c0fb8eecc4e
zSnails <erizojuan33@gmail.com>
2024-11-04 17:02:48 -0600

    fix: now the chat button shows the user display name

--- components/ChatButton.vue
+++ components/ChatButton.vue
@@ -2,20 +2,25 @@
   <NuxtLink class="flex flex-row items-center justify-start gap-2 h-fit p-2" :href="`/chats/${id}`">
     <div class="avatar">
       <div class="w-10 rounded-full">
-        <img v-if="img" :src="img" :alt="`${email}'s profile picture`">
+        <img v-if="user.img" :src="user.img" :alt="`${user.img}'s profile picture`">
         <img v-else
           src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
           alt="">
       </div>
     </div>
     <div class="text-2xl font-normal">
-      <p v-if="email">{{ email }}</p>
+      <p v-if="meta.display_name">{{ meta.display_name }}</p>
+      <p v-else-if="user.email">{{ user.email }}</p>
       <p v-else>[NAME]</p>
     </div>
   </NuxtLink>
 </template>
 <script setup lang="ts">
 import type { User } from '~/user';
+interface Meta {
+  display_name?: string;
+}
 
-const { id, email, img } = defineProps<User>();
+const { user, id } = defineProps<{ user: User, id: number }>();
+const meta = user.raw_user_meta_data as Meta;
 </script>
