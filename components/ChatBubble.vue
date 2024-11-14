ccf375d74b1537a95da7495df236a3300b81c2b3
zSnails <erizojuan33@gmail.com>
2024-11-14 14:31:30 -0600

    feat: message deletion

--- components/ChatBubble.vue
+++ components/ChatBubble.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :id="`${msg.sent_by}-${msg.conversation}-${msg.id}`" class="chat"
+  <div ref="self" :id="`${msg.sent_by}-${msg.conversation}-${msg.id}`" class="chat"
     :class="{ 'chat-start': msg.sent_by !== user?.id, 'chat-end': msg.sent_by === user?.id }">
     <div v-if="msg.media" class="chat-header">
       <div v-if="loading"
@@ -13,23 +13,52 @@
         <source :src="msg.media" type="video/mp4">
       </video>
     </div>
-    <div class="chat-bubble" v-if="msg.content">{{ msg.content }}</div>
+    <div v-if="msg.sent_by === user?.id && msg.content" :onclick="`deletion_modal_${msg.id}.showModal()`"
+      class="chat-bubble">{{ msg.content }}</div>
+    <div v-else-if="msg.content" class="chat-bubble" v-if="msg.content">{{ msg.content
+      }}
+    </div>
+    <!-- <button class="btn" @click.right.prevent="`deletion_modal_${msg.id}.showModal()`">open modal</button> -->
     <div class="chat-footer">
       {{ formatDistance(msg.created_at, new Date(), { addSuffix: true }) }}
     </div>
     <dialog v-if="msg.media" :id="`img_${msg.id}_modal`" class="modal">
       <img class="modal-box" :src="msg.media">
     </dialog>
+
+    <dialog :id="`deletion_modal_${msg.id}`" v-if="msg.sent_by === user?.id" class="modal">
+      <div class="modal-box flex flex-col items-center">
+        <form method="dialog">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
+        <button @click="() => deleteMessage(msg.id.toString())" class="text-white btn btn-wide btn-error">
+          <TrashIcon class="size-6"></TrashIcon>
+          Delete
+        </button>
+      </div>
+    </dialog>
+
   </div>
 </template>
 <script setup lang="ts">
 import { formatDistance } from 'date-fns';
+import { TrashIcon } from '@heroicons/vue/16/solid';
 import type { Message } from '~/message';
 
+const self = ref<HTMLDivElement>() as Ref<HTMLDivElement>;
 const video = ref(false);
 const loading = ref(true);
 const loaded = () => {
   loading.value = false;
+};
+
+const deleteMessage = async (id: string) => {
+  try {
+    await $fetch(`/api/messages/${id}`, { method: "DELETE" });
+    self.value.remove();
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 const user = useSupabaseUser();
