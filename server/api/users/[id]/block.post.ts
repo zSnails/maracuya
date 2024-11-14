d93decb4d3724fa84c5fb504715ce5cd232b9b3f
zSnails <erizojuan33@gmail.com>
2024-11-14 15:31:23 -0600

    fix: check if the user has already been blocked

--- server/api/users/[id]/block.post.ts
+++ server/api/users/[id]/block.post.ts
@@ -7,6 +7,14 @@ export default defineEventHandler(async (event: H3Event) => {
   const me = (await serverSupabaseUser(event))!!;
   const supabase = await serverSupabaseClient<Database>(event);
 
+  const { count } = await supabase.from("blocked_users").select("*", { head: true, count: 'exact' }).eq("user_id", me.id).eq("blocked_user_id", toBlock);
+  if (count && count > 0) {
+    throw createError({
+      statusCode: 400,
+      message: "this user has already been blocked by you"
+    });
+  }
+
   const { error, data } = await supabase.from("blocked_users").insert({
     user_id: me.id,
     blocked_user_id: toBlock,
